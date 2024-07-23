73b07141cc58451cb5e666c19afa51ffa132419b
aryakvipin <arya28985@gmail.com>
2024-07-23 11:01:20 +0530

    Add all my files

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_with_api/controller/bottom_nav_controller.dart';
+import 'package:news_app_with_api/controller/category_controller.dart';
+import 'package:news_app_with_api/controller/home-screen_controller.dart';
+import 'package:news_app_with_api/controller/search_screen_controller.dart';
+import 'package:news_app_with_api/controller/splash_controller.dart';
+import 'package:news_app_with_api/view/bottom_nav_bar/Main_page.dart';
+import 'package:news_app_with_api/view/splashScreen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => SplashScreenProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => HomeScreenController(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SearchScreenController(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => BottomNavController(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => CategoryController(),
+          )
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Consumer<SplashScreenProvider>(
+            builder: (context, splashScreenProvider, widget) {
+              return splashScreenProvider.isLoading
+                  ? SplashScreen()
+                  : MainPage(); // Replace with your home screen
+            },
+          ),
+        ));
+  }
+}
---
+++ lib/view/home_screen/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:news_app_with_api/controller/home-screen_controller.dart';
+import 'package:news_app_with_api/utils/textTheme.dart';
+import 'package:news_app_with_api/view/search_screen/search_screen.dart';
+import 'package:news_app_with_api/view/widgets/news_card.dart';
+import 'package:provider/provider.dart';
+class HomeScreen extends StatelessWidget {
+  void fetchData(BuildContext context) {
+    Provider.of<HomeScreenController>(context, listen: false)
+        .fetchData();
+  }
+  @override
+  Widget build(BuildContext context) {
+    fetchData(context);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff6A3DE8),
+          elevation: 0,
+          title: const Text(
+            "News Today 📰",
+          ),
+        titleTextStyle: NewsText.textMain,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SearchScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.search),
+            )
+          ],
+        ),
+        body: Consumer<HomeScreenController>(
+            builder: (context, provider, child) {
+          return provider.isLoading == true
+              ? Center(
+                  child: LottieBuilder.asset(
+                  "assets/animations/Animation - 1702395258490 (2).json",
+                  fit: BoxFit.cover,
+                  height: 150,
+                  width: 150,
+                ))
+              : Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ListView.separated(
+                      itemBuilder: (context, index) => NewsCard(
+                            title: provider.newsModel.articles?[index].title.toString() ?? "",
+                            description: provider.newsModel.articles?[index].description.toString() ?? "",
+                            date: provider.newsModel.articles?[index].publishedAt,
+                            imageUrl: provider.newsModel.articles?[index].urlToImage.toString() ?? "",
+                            contant: provider.newsModel.articles?[index].content.toString() ?? "",
+                            sourceName: provider.newsModel.articles?[index].source?.name.toString() ?? "",
+                            url: provider.newsModel.articles?[index].url.toString() ?? "",
+                          ),
+                      separatorBuilder:
+                          (context, index) => const Divider(
+                            height: 20,
+                          ),
+                      itemCount: provider.newsModel.articles?.length ?? 0),
+                );
+        }));
+  }
+}
